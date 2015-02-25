398ec7e08589b64bb7ab12acb0327a2b998c34ef
James Hartig <fastest963@gmail.com>
2015-02-25 17:17:59 -0500

    WIP of initial v0.3.0
    - DelimiterStream.wrap is the new way things will work
    - new DelimiterStream(stream) is deprecated now
    - All tests still haven't been converted yet
    - Initial support of non-node streams (i.e. WebSockets) via wrap

---
+++ tests/lib/DataEmitter.js
@@ -0,0 +1,71 @@
+var util = require('util'),
+    events = require('events');
+
+function DataEmitter(enc, n, i) {
+    events.EventEmitter.apply(this);
+
+    n = n || 1000;
+    this.buffer = new Buffer(n);
+    this.buffer.fill(48); //"0"
+    this.position = 0;
+    this.max = n;
+    this.interval = i || 250; //interval which causes a break in the read loop
+    this._readableState = {encoding: null};
+    this.done = false;
+    if (enc) {
+        this.setEncoding(enc);
+    }
+}
+util.inherits(DataEmitter, events.EventEmitter);
+
+DataEmitter.prototype.write = function() {
+    if (this._readableState.encoding) {
+        this.buffer.write.apply(this.buffer, Array.prototype.slice.call(arguments, 0));
+    } else {
+        this.buffer[arguments[1]] = arguments[0];
+    }
+};
+DataEmitter.prototype.setEncoding = function(encoding) {
+    this._readableState.encoding = encoding;
+};
+
+DataEmitter.prototype.read = function(n) {
+    if (this.position >= this.max) {
+        this.done = true;
+        this.emit('done');
+        return null;
+    }
+
+    n = n || this.max;
+    var start = this.position;
+    while (this.position++ < this.max && (this.position - start) <= n) {
+        if ((this.position % this.interval) === 0) {
+            break;
+        }
+    }
+    var data;
+    if (this._readableState.encoding) {
+        data = this.buffer.slice(start, this.position).toString(this.encoding);
+    } else {
+        data = this.buffer.slice(start, this.position);
+    }
+    this.emit('data', data);
+    process.nextTick(function() {
+        //don't trigger readable if we're already done
+        if (this.done) {
+            return;
+        }
+        this.read();
+    }.bind(this));
+};
+DataEmitter.prototype.begin = function() {
+    process.nextTick(function() {
+        this.read();
+    }.bind(this));
+};
+DataEmitter.prototype.close = function() {
+    process.nextTick(function() {
+        this.emit('close');
+    }.bind(this));
+};
+module.exports = DataEmitter;
